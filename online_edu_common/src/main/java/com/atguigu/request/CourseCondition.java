75bf510c35f4e9df669d4fc1faefcd233b46d0b4
肖雄辉 <1292070934@qq.com>
2020-11-13 19:25:25 +0800

    完成了阿里云视频删除

---
+++ online_edu_common/src/main/java/com/atguigu/request/CourseCondition.java
@@ -0,0 +1,12 @@
+package com.atguigu.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class CourseCondition {
+    @ApiModelProperty(value = "课程状态 Draft未发布  Normal已发布")
+    private String status;
+    @ApiModelProperty(value = "课程标题")
+    private String title;
+}
